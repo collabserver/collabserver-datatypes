c712a4abd655ed2c626b8491478ccfa3d72127fb
geekymoose <constantin.masson@umontreal.ca>
2018-05-17 17:32:36 -0400

    Remove all examples CmRDT (Replaced by unit tests)

--- examples/main.cpp
+++ examples/main.cpp
@@ -7,16 +7,6 @@
 #include "CvRDT/GMap_Example.h"
 #include "CvRDT/GGraph_Example.h"
 
-// CmRDT (Operation-based)
-#include "CmRDT/LWWGraph_Example.h"
-#include "CmRDT/LWWMap_Example.h"
-#include "CmRDT/LWWRegister_Example.h"
-#include "CmRDT/LWWSet_Example.h"
-
-// collab
-#include "collab/LWWViewMDE_Example.h"
-
-
 using namespace CRDT;
 
 
@@ -35,19 +25,5 @@ int main(int argc, char** argv) {
     CvRDT::GMap_example();
 
     CvRDT::GGraph_example();
-
-
-    // CmRDT (Operation-based)
-
-    CmRDT::LWWMap_example();
-    CmRDT::LWWRegister_example();
-    CmRDT::LWWSet_example();
-    CmRDT::LWWGraph_example();
-
-
-    // collab
-    collab::LWWViewMDE_example();
-
-
     return 0;
 }
