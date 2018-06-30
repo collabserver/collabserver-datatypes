6fa044c190805dc6d9c285018ee214043e256eaf
geekymoose <constantin.masson@umontreal.ca>
2018-06-29 20:00:01 -0400

    Remove CMake COLLABDATA_LIBRARY (Was not actually useful)

--- CMake/AddCustom.cmake
+++ CMake/AddCustom.cmake
@@ -1,6 +1,5 @@
 include_directories("${CMAKE_SOURCE_DIR}/include/")
 file(GLOB_RECURSE srcFilesCustom "${CMAKE_SOURCE_DIR}/src/*.cpp")
 add_library(collabdata STATIC ${srcFilesCustom})
-set(COLLABDATA_LIBRARY collabdata)
 
 
