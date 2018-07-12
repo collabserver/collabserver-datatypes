d219337d8e2868ae11c5f4216b45853b47bb8b31
geekymoose <constantin.masson@umontreal.ca>
2018-07-12 15:00:16 -0400

    Add tests for simple graph operations notifications

--- test/custom/Test_SimpleGraph.cpp
+++ test/custom/Test_SimpleGraph.cpp
@@ -14,21 +14,48 @@ namespace collab {
 // *****************************************************************************
 // /////////////////////////////////////////////////////////////////////////////
 
+
+static int nbOpVertexAdd = 0;
+static int nbOpVertexRemove = 0;
+static int nbOpEdgeAdd = 0;
+static int nbOpEdgeRemove = 0;
+static int nbOpAttrAdd = 0;
+static int nbOpAttrRemove = 0;
+static int nbOpAttrSet = 0;
+
+static void resetNbOpCounters() {
+    nbOpVertexAdd = 0;
+    nbOpVertexRemove = 0;
+    nbOpEdgeAdd = 0;
+    nbOpEdgeRemove = 0;
+    nbOpAttrAdd = 0;
+    nbOpAttrRemove = 0;
+    nbOpAttrSet = 0;
+}
+
+
 class HandlerMock : public SimpleGraph::OpHandler {
     public:
         void handleOperation(const SimpleGraph::VertexAddOperation& op) {
+            nbOpVertexAdd++;
         }
         void handleOperation(const SimpleGraph::VertexRemoveOperation& op) {
+            nbOpVertexRemove++;
         }
         void handleOperation(const SimpleGraph::EdgeAddOperation& op) {
+            nbOpEdgeAdd++;
         }
         void handleOperation(const SimpleGraph::EdgeRemoveOperation& op) {
+            nbOpEdgeRemove++;
         }
         void handleOperation(const SimpleGraph::AttributeAddOperation& op) {
+            nbOpAttrAdd++;
         }
         void handleOperation(const SimpleGraph::AttributeRemoveOperation& op) {
+            nbOpAttrRemove++;
         }
         void handleOperation(const SimpleGraph::AttributeSetOperation& op) {
+            nbOpAttrSet++;
         }
 };
 
@@ -58,8 +85,44 @@ TEST(SimpleGraph, constructorTest) {
     // TODO
 }
 
+TEST(SimpleGraph, operationHandlersTest) {
+    SimpleGraph data0;
+    ObserverMock obs;
+    data0.addOperationObserver(obs);
 
-// TODO
+    resetNbOpCounters();
+
+    data0.addVertex("v1");
+    data0.addVertex("v1");
+    data0.addVertex("v1");
+    data0.addVertex("v1");
+    ASSERT_EQ(nbOpVertexAdd, 1);
+
+    data0.addVertex("v2");
+    data0.addVertex("v2");
+    data0.addVertex("v2");
+    ASSERT_EQ(nbOpVertexAdd, 2);
+
+    data0.addVertex("v3");
+    data0.addVertex("v3");
+    data0.addVertex("v3");
+    ASSERT_EQ(nbOpVertexAdd, 3);
+
+    data0.addVertex("v3");
+    data0.addVertex("v2");
+    data0.addVertex("v1");
+    ASSERT_EQ(nbOpVertexAdd, 3);
+
+    data0.removeEdge("v1", "v2");
+    data0.removeEdge("v1", "v2");
+    data0.removeEdge("v1", "v2");
+    ASSERT_EQ(nbOpEdgeRemove, 0);
+
+    data0.addEdge("v1", "v2");
+    data0.addEdge("v1", "v2");
+    data0.addEdge("v1", "v2");
+    ASSERT_EQ(nbOpEdgeAdd, 1);
+}
 
 
 } // End namespace
