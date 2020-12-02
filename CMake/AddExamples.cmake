fa7060abd468cba907999387e7ff55a6cd9c05c7
geekymoose <constantin.masson@umontreal.ca>
2020-12-02 01:37:18 +0100

    Fix missing CMAKE_CURRENT_x value in CMake so that it doesn't conflict in other libs

--- CMake/AddExamples.cmake
+++ CMake/AddExamples.cmake
@@ -1,6 +1,6 @@
-include_directories("${CMAKE_SOURCE_DIR}/include/")
+include_directories("${CMAKE_CURRENT_SOURCE_DIR}/include/")
 
 # CmRDT examples
-add_executable(examplesCmRDT "${CMAKE_SOURCE_DIR}/examples/CmRDT/runAllExamples.cpp")
+add_executable(examplesCmRDT "${CMAKE_CURRENT_SOURCE_DIR}/examples/CmRDT/runAllExamples.cpp")
 add_custom_target(runExamplesCmRDT examplesCmRDT)
 
