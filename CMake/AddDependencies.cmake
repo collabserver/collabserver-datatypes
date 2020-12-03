6b72ab5ef3f490610ea676217b71a699b5078951
geekymoose <constantin.masson@umontreal.ca>
2020-12-03 13:59:58 +0100

    Upgrade to googletest release-1.10.0

--- CMake/AddDependencies.cmake
+++ CMake/AddDependencies.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY  https://github.com/google/googletest.git
-    GIT_TAG         release-1.8.1
+    GIT_TAG         release-1.10.0
     SOURCE_DIR      "${CMAKE_CURRENT_SOURCE_DIR}/extern/googletest"
 )
 FetchContent_MakeAvailable(googletest)
