b03d8282db5d1522be06fa29700c80947f8103a0
geekymoose <constantin.masson@umontreal.ca>
2020-12-04 02:34:08 +0100

    Update CMake build system to avoid conflicts when used as dependency

--- CMake/AddExamples.cmake
+++ CMake/AddExamples.cmake
@@ -1,6 +1,4 @@
-include_directories("${CMAKE_CURRENT_SOURCE_DIR}/include/")
-
-# CmRDT examples
-add_executable(examplesCmRDT "${CMAKE_CURRENT_SOURCE_DIR}/examples/CmRDT/runAllExamples.cpp")
-add_custom_target(runExamplesCmRDT examplesCmRDT)
+include_directories("${PROJECT_SOURCE_DIR}/include/")
+add_executable(${PROJECT_NAME}-examplesCmRDT "${PROJECT_SOURCE_DIR}/examples/CmRDT/runAllExamples.cpp")
+add_custom_target(runExamplesCmRDT ${PROJECT_NAME}-examplesCmRDT)
 
--- CMake/AddTests.cmake
+++ CMake/AddTests.cmake
@@ -1,15 +1,15 @@
 enable_testing()
 
 # Tests executable
-include_directories("${CMAKE_CURRENT_SOURCE_DIR}/include/")
-file(GLOB_RECURSE srcFilesTests "${CMAKE_CURRENT_SOURCE_DIR}/tests/*.cpp")
-add_executable(tests ${srcFilesTests})
+include_directories("${PROJECT_SOURCE_DIR}/include/")
+file(GLOB_RECURSE srcFilesTests "${PROJECT_SOURCE_DIR}/tests/*.cpp")
+add_executable(${PROJECT_NAME}-tests ${srcFilesTests})
 
 # Googletest dependency
-include_directories("${CMAKE_CURRENT_SOURCE_DIR}/extern/googletest/googletest/include/")
-target_link_libraries(tests gtest)
+include_directories("${PROJECT_SOURCE_DIR}/extern/googletest/googletest/include/")
+target_link_libraries(${PROJECT_NAME}-tests gtest)
 
 # Tests target
-add_test(NAME googletests COMMAND tests)
-add_custom_target(runTests tests)
+add_test(NAME googletests COMMAND ${PROJECT_NAME}-tests)
+add_custom_target(runTests ${PROJECT_NAME}-tests)
 
