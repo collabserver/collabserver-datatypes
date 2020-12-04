487c195d357ab3896baef4ae12889a9f78304ed9
geekymoose <constantin.masson@umontreal.ca>
2020-12-04 02:52:58 +0100

    Simplify the CMake (place everything in the CMakeLists.txt since it is small enough)

--- CMake/AddExamples.cmake
+++
@@ -1,4 +0,0 @@
-include_directories("${PROJECT_SOURCE_DIR}/include/")
-add_executable(${PROJECT_NAME}-examplesCmRDT "${PROJECT_SOURCE_DIR}/examples/CmRDT/runAllExamples.cpp")
-add_custom_target(runExamplesCmRDT ${PROJECT_NAME}-examplesCmRDT)
-
